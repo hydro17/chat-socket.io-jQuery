1d4cf46480ce2294e58e96d38bf3114ddcb30a63
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-05-22 12:05:50 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+app.use(express.static('public'))
+
+io.on('connection', (socket) => {
+    console.log("client app connected");
+  }
+);
+
+http.listen(3000, () => console.log("listening on port: 3000..."));
