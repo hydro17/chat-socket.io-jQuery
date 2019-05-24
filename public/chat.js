5759848fae6b778627ab95b08aa601f8bc1caa49
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-05-24 23:55:09 +0200

    add scrolling messages using keyboard arrows up and down | add scrolling to the bottom of messages window after adding a new message

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,7 @@
 // const socket = io("http://localhost:3000");
 $(function() {
   const newMessage = $("#newMessage");
+  const messages = $("#messages")[0];
 
   const socket = io();
   socket.on('connect', () => console.log(`socket.id: ${socket.id}`));
@@ -18,7 +19,16 @@ $(function() {
     e.preventDefault();
     const message = newMessage.val();
     $('#messages').append(`<li>${message}</li>`);
+
+    const isFullyScrolledToBottom = messages.scrollHeight - (messages.offsetHeight + messages.scrollTop) < 40; 
+    if (isFullyScrolledToBottom) { messages.scrollTop = messages.scrollHeight };
+
     socket.emit('chat message', message);
     newMessage.val('');
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') messages.scrollTop -= 10;
+    if (e.key === 'ArrowDown') messages.scrollTop += 10;
   });
 });
