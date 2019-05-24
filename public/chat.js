6444d46ed3cd8599d4eeae5ee40b8e79affd8ba9
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-05-25 00:12:46 +0200

    refactor 'chat.js'

--- public/chat.js
+++ public/chat.js
@@ -1,15 +1,14 @@
 // const socket = io("http://localhost:3000");
 $(function() {
-  const newMessage = $("#newMessage");
   const messages = $("#messages")[0];
 
   const socket = io();
   socket.on('connect', () => console.log(`socket.id: ${socket.id}`));
   socket.on('chat message', (message) => $('#messages').append(`<li>${message}</li>`));
 
-  newMessage[0].onkeyup = (e) => {
-    if (e.key === 'Enter') e.target.dispatchEvent(new Event('submit'))
-  };
+  $("#newMessage").on('keyup', (e) => {
+    if (e.key === 'Enter') { e.target.dispatchEvent(new Event('submit')) }
+  });
 
   // newMessage[0].onkeyup = (e) => {
   //   if (e.key === 'Enter') $(e.target).submit();
@@ -17,14 +16,14 @@ $(function() {
 
   $('form').submit((e) => {
     e.preventDefault();
-    const message = newMessage.val();
+    const message = $("#newMessage").val();
     $('#messages').append(`<li>${message}</li>`);
 
     const isFullyScrolledToBottom = messages.scrollHeight - (messages.offsetHeight + messages.scrollTop) < 40; 
     if (isFullyScrolledToBottom) { messages.scrollTop = messages.scrollHeight };
 
     socket.emit('chat message', message);
-    newMessage.val('');
+    $("#newMessage").val('');
   });
 
   window.addEventListener('keydown', (e) => {
