e43ebc7f52a68d43cb2c7d92b482fc531e4f07b1
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-05-25 18:34:21 +0200

    add nicknames | refactor chat | update styles

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 let connectionsCount = 0;
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
-app.use(express.static('public'))
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.use(express.static("public"));
 
-io.on('connection', (socket) => {
-    console.log(`a user connected - number of connections: ${++connectionsCount} at ${new Date().toLocaleString()}`);
+io.on("connection", (socket) => {
+  console.log(
+    `a user connected - number of connections: ${++connectionsCount} at ${new Date().toLocaleString()}`
+  );
 
-    socket.on('disconnect', () => console.log("a user disconnected"));
-    socket.on('chat message', (message) => socket.broadcast.emit('chat message', message));
-  }
-);
+  socket.on("disconnect", () => console.log("a user disconnected"));
+  socket.on("chat message", (message) =>
+    io.emit("chat message", `<strong>${message.nickname}</strong>: ${message.content}`)
+  );
+});
 
 http.listen(3000, () => console.log("listening on port: 3000..."));
-q
--- public/chat.js
+++ public/chat.js
@@ -1,33 +1,57 @@
 // const socket = io("http://localhost:3000");
 $(function() {
   const messages = $("#messages")[0];
+  let nickname;
 
+  // eslint-disable-next-line no-undef
   const socket = io();
-  socket.on('connect', () => console.log(`socket.id: ${socket.id}`));
-  socket.on('chat message', (message) => $('#messages').append(`<li>${message}</li>`));
+  socket.on("connect", onConnect);
+  socket.on("disconnect", () => console.log(`disconnected`));
+  socket.on("chat message", onChatMessage);
 
-  $("#newMessage").on('keyup', (e) => {
-    if (e.key === 'Enter') { e.target.dispatchEvent(new Event('submit')) }
+  function onConnect() {
+    console.log(`socket.id: ${socket.id}`);
+    nickname = sessionStorage.getItem("nickname");
+    if (!nickname) {
+      nickname = prompt("Type in your nickname:", "anonymous") || "anonymous";
+      sessionStorage.setItem("nickname", nickname);
+    }
+    $("#nickname").html(nickname);
+  }
+
+  function onChatMessage(message) {
+    $("#messages").append(`<li>${message}</li>`);
+
+    const isFullyScrolledToBottom =
+      messages.scrollHeight - (messages.offsetHeight + messages.scrollTop) < 40;
+
+    if (isFullyScrolledToBottom) {
+      messages.scrollTop = messages.scrollHeight;
+    }
+  }
+
+  $("#newMessage").on("keyup", (e) => {
+    if (e.key === "Enter") {
+      e.target.dispatchEvent(new Event("submit"));
+    }
   });
 
   // newMessage[0].onkeyup = (e) => {
   //   if (e.key === 'Enter') $(e.target).submit();
   // };
 
-  $('form').submit((e) => {
+  $("form").submit((e) => {
     e.preventDefault();
-    const message = $("#newMessage").val();
-    $('#messages').append(`<li>${message}</li>`);
 
-    const isFullyScrolledToBottom = messages.scrollHeight - (messages.offsetHeight + messages.scrollTop) < 40; 
-    if (isFullyScrolledToBottom) { messages.scrollTop = messages.scrollHeight };
-
-    socket.emit('chat message', message);
-    $("#newMessage").val('');
+    socket.emit("chat message", {
+      nickname,
+      content: $("#newMessage").val(),
+    });
+    $("#newMessage").val("");
   });
 
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowUp') messages.scrollTop -= 10;
-    if (e.key === 'ArrowDown') messages.scrollTop += 10;
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp") messages.scrollTop -= 10;
+    if (e.key === "ArrowDown") messages.scrollTop += 10;
   });
 });
